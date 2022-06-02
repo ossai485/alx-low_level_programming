efc74d91abdbe40d51855d5214295ba4be23d28d
Etim Augustine <etimaugustine485@gmail.com>
2022-06-02 13:41:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,15 +4,13 @@
 
 /**
 
- * main - Entry point
+ *  main - Entry point
 
- *
+ *  Description: prints "\"Programming is like building a multilingual puzzle"
 
- * Return: Always 0 (Success)
+ *  Return: 0
 
  */
-
-
 
 int main(void)
 
