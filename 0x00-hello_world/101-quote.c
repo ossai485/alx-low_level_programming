563c8a424cf9e843dd159bb781abc4cd5b4107ff
Etim Augustine <etimaugustine485@gmail.com>
2022-06-02 04:13:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
 int main(void)
 
 {
@@ -21,4 +8,5 @@ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 
 return (1);
+
 }
