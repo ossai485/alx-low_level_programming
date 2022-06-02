18795b3ebf02dc7d8c52f7128e2943ed29dca2d6
Etim Augustine <etimaugustine485@gmail.com>
2022-06-02 14:04:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
-
- *  main - Entry point
-
- *  Description: prints "\"Programming is like building a multilingual puzzle"
-
- *  Return: 0
-
- */
-
+*    main - Entry point
+*    Return: 0
+*/
 int main(void)
-
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
-
+*    Return: 0
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
