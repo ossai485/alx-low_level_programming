bc5448ea58666494e92e4e759f1aab7a8d6a558b
Etim Augustine <etimaugustine485@gmail.com>
2022-06-03 06:57:30 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,16 +9,13 @@
 int main(void)
 {
 char low, e, q;
-
-e = e;
-q = q;
-
-for (low = a; low <= z; low++)
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
 {
 if (low != e && low != q)
 putchar(low);
 }
-printf("\n");
-
+printf(n);
 return (0);
 }
