39759d0a788603d338f73bd4a67af22debdb6380
Etim Augustine <etimaugustine485@gmail.com>
2022-06-07 12:03:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+include stdio.h
+/**
+ * main Entry point
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+int n, co;
+co = 0;
+while (co < 10)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+co++;
+putchar('\n');
+}
+}
+
