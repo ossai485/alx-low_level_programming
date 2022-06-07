021bce9e913c4269bd9c527c879e3a259a4147ef
Etim Augustine <etimaugustine485@gmail.com>
2022-06-07 08:42:23 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char *sh = _putchar;
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
+}
