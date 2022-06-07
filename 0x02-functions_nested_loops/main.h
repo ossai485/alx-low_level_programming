5d528d10d2efa760a64fde8b4384577f3d07fa48
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-06-07 23:34:37 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,13 @@
-#include "stdio.h"
-/**
- * print_alphabet - a function that prints the alphabet, in lowercast
- *
- * Return: Always 0 (Succes)
- */
-void print_alphabet(void)
-{
-int n;
-for (n = 'a'; n <= 'z'; n++)
-{
-putchar(n);
-}
-putchar('\n');
-}
+int _putchar(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
