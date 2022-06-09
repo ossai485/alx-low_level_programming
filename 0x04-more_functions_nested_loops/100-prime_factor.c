ac7b3c674c75d162a581c17b80fcf441d8028199
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-06-09 16:21:49 +0100

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,5 +8,18 @@ ble File  10 lines (10 sloc)  139 Bytes
  */
 int main(void)
 {
+	long x, y;
+
+	x = 612852475143;
+
+	for (y = 2; x > y; y++)
+	{
+		while (x % y == 0)
+		{
+			x = x / y;
+		}
+	}
+	printf("%lu", y);
+	putchar('\n');
 	return (0);
 }
