2d9e55e7a5420cb739840b446254d85cd312ccac
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-06-09 16:14:59 +0100

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+ble File  10 lines (10 sloc)  139 Bytes
+   
+#include <stdio.h>
+/**
+ * main - prints prime factors of 612852475143
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
