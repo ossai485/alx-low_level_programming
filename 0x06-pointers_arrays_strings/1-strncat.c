2b7c9b6b40596573c4571bc9a1fb3bad4f071382
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-06-15 23:18:06 +0100

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,41 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strncat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- * @n: times to iterations
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return:
+ * Return: dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int a;
-	int b;
+	int i;
+	int j;
 
-	a = 0;
-
-		while (dest[a] != 0)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		a++;
+		i++;
 	}
-
-	b = 0;
-
-		while (src[b] != 0 && b < n)
-		{
-			dest[a] = src[b];
-			a++;
-			b++;
-		}
-
-		if (n > 0)
-
-		{
-			dest[a] = 0;
-		}
-
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
