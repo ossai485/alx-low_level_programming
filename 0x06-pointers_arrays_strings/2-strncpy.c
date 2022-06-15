711455823c40802f15d5239416db8c2b5394b2be
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-06-15 23:08:40 +0100

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,17 +14,17 @@ char *_strncpy(char *dest, char *src, int n)
 
 	i = 0;
 
-	while(i < n && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
 	i = i;
-	while(i < n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
-	return dest;
+	return (dest);
 }
