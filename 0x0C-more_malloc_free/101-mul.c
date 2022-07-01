48338361d8bcbb6cc009e0acdcc8635bebba4493
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-07-01 03:26:56 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,156 +1,132 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * str_len - finds string length
- * @str: input pointer to string
- * Return: length of string
- */
-int str_len(char *str)
-{
-	int len;
 
-	for (len = 0; *str != '\0'; len++)
-		len++, str++;
-	return (len / 2);
-}
 /**
- * _calloc - allocates memory for an array using malloc
- * @bytes: bytes of memory needed per size requested
- * @size: size in bytes of each element
- * Return: pointer to the allocated memory
+ * adding_all_mul - sum all the addition to know the multiplication result
+ * @a: number 1
+ * @len_a: lenght of number 1
+ * @b: number 2
+ * @len_b: lenght of number 2
+ * Return: Addition pointer to the total resul of the  multiplication
  */
-void *_calloc(unsigned int bytes, unsigned int size)
+add_t *adding_all_mul(char *a, int len_a, char *b, int len_b)
 {
-	unsigned int i;
-	char *p;
+	add_t *result = NULL;
+	int i = 0, j = 0, carry = 0;
 
-	if (bytes == 0 || size == 0)
-		return (NULL);
-	if (size >= UINT_MAX / bytes || bytes >= UINT_MAX / size)
-		return (NULL);
-	p = malloc(size * bytes);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < bytes * size; i++)
-		p[i] = 0;
-	return ((void *)p);
-}
-/**
- * add_arrays - adds 2 arrays of ints
- * @mul_result: pointer to array with numbers from product
- * @sum_result: pointer to array with numbers from total sum
- * @len_r: length of both arrays
- * Return: void
- */
-void add_arrays(int *mul_result, int *sum_result, int len_r)
-{
-	int i = 0, len_r2 = len_r - 1, carry = 0, sum;
+	result = malloc(sizeof(add_t));
 
-	while (i < len_r)
+	result->next = NULL, result->n_dig = 0, result->len_r = len_a + len_b;
+
+	result->n_add = malloc(sizeof(char) * result->len_r);
+
+	for (i = 0; i < result->len_r; i++)
+		result->n_add[i] = '0';
+
+	for (i = len_a - 1; i >= 0; i--)
 	{
-		sum = carry + mul_result[len_r2] + sum_result[len_r2];
-		sum_result[len_r2] = sum % 10;
-		carry = sum / 10;
-		i++;
-		len_r2--;
-	}
-}
-/**
- * is_digit - checks for digits
- * @c: input character to check for digit
- * Return: 0 failure, 1 success
- */
-int is_digit(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	printf("Error\n");
-	return (0);
-}
-/**
- * multiply - multiplies 2 #'s, prints result, must be 2 #'s
- * @num1: factor # 1 (is the smaller of 2 numbers)
- * @len_1: length of factor 1
- * @num2: factor # 2 (is the larger of 2 numbers)
- * @len_2: length of factor 2
- * @len_r: length of result arrays
- * Return: 0 fail, 1 success
- */
-int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r)
-{
-	int i = 0, i1 = len_1 - 1;
-	int i2, product, carry, digit, *mul_result, *sum_result;
-
-	sum_result = _calloc(sizeof(int), (len_r));
-	while (i < len_1)
-	{
-		mul_result = _calloc(sizeof(int), len_r);
-		i2 = len_2 - 1, digit = (len_r - 1 - i);
-		if (!is_digit(num1[i1]))
-			return (NULL);
 		carry = 0;
-		while (i2 >= 0)
+		for (j = len_b - 1; j >= 0; j--)
 		{
-			if (!is_digit(num2[i2]))
-				return (NULL);
-			product = (num1[i1] - '0') * (num2[i2] - '0');
-			product += carry;
-			mul_result[digit] += product % 10;
-			carry = product / 10;
-			digit--, i2--;
+			carry += (a[i] - '0') * (b[j] - '0');
+			carry += result->n_add[i + j + 1] - '0';
+
+			result->n_add[i + j + 1] = (carry % 10) + '0';
+			carry /= 10;
 		}
-		add_arrays(mul_result, sum_result, len_r);
-		free(mul_result);
-	    i++, i1--;
+		if (carry)
+			result->n_add[i + j + 1] = (carry % 10) + '0';
 	}
-	return (sum_result);
+	if (result->n_add[0] != '0')
+		result->n_dig = len_a + len_b;
+	else
+		result->n_dig = len_a + len_b - 1;
+
+	return (result);
 }
+
 /**
- * print_me - prints my array of the hopeful product here
- * @sum_result: pointer to int array with numbers to add
- * @len_r: length of result array
- * Return: void
+ * print_free_result - print the result of the multiplication and free all
+ * @result: Addition pointer to the total resul of the  multiplication
+ * Result: Nothing
  */
-void print_me(int *sum_result, int len_r)
+void print_free_result(add_t *result)
 {
+	int i = 0, start_n = 0;
+
+	i = 0;
+	while (i < result->n_dig)
+	{
+		if (start_n || result->n_add[result->len_r - result->n_dig + i] != '0')
+		{
+			_putchar(result->n_add[result->len_r - result->n_dig + i]);
+			start_n = 1;
+		}
+		i++;
+	}
+	if (!result->n_dig || !start_n)
+		_putchar('0');
+	_putchar('\n');
+	free(result->n_add);
+	free(result);
+}
+
+/**
+ * error_message - print an error message and exit with status 98
+ * Return: Nothing
+ */
+void error_message(void)
+{
+	char error_msg[] = "Error";
 	int i = 0;
 
-	while (sum_result[i] == 0 && i < len_r)
+	while (error_msg[i] != '\0')
+	{
+		_putchar(error_msg[i]);
 		i++;
-	if (i == len_r)
-		_putchar('0');
-	while (i < len_r)
-		_putchar(sum_result[i++] + '0');
-	_putchar('\n');
-}
-/**
- * main - multiply 2 input #'s of large lengths and print result or print Error
- * @argc: input count of args
- * @argv: input array of string args
- * Return: 0, Success
- */
-int main(int argc, char **argv)
-{
-	int len_1, len_2, len_r, temp, *sum_result;
-	char *num1, *num2;
+	}
 
-	if (argc != 3)
+	_putchar('\n');
+
+	exit(98);
+}
+
+/**
+ * main - multiply 2 long numbers
+ * usage <> ./mul num1 num2
+ * @ac: number of arguments
+ * @av: list of arguments
+ * Return: 0 on success, another number otherwise
+ */
+int main(int ac, char **av)
+{
+	char *a = NULL, *b =  NULL;
+	int i = 0, len_a = 0, len_b = 0, is_a = 1, is_b = 1, len_r = 0;
+	add_t *result = NULL;
+
+	if (ac != 3)
+		error_message();
+
+	for (i = 0, a = av[1], b = av[2]; is_a == 1 || is_b == 1; i++)
 	{
-		printf("Error\n");
-		exit(98);
+		if (is_a == 1 && a[i] == '\0')
+			is_a = 0, len_a = i;
+		if (is_b == 1 && b[i] == '\0')
+			is_b = 0, len_b = i;
+		if ((is_a == 1 && (a[i] < '0' || a[i] > '9')) ||
+				(is_b == 1 && (b[i] < '0' || b[i] > '9')))
+			error_message();
 	}
-	len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
-	len_r = len_1 + len_2;
-	if (len_1 < len_2)
-		num1 = argv[1], num2 = argv[2];
-	else
-	{
-		num1 = argv[2], num2 = argv[1];
-		temp = len_2, len_2 = len_1, len_1 = temp;
-	}
-	sum_result = multiply(num1, len_1, num2, len_2, len_r);
-	if (sum_result == NULL)
-		exit(98);
-	print_me(sum_result, len_r);
+
+	if (len_a == 0 || len_b == 0)
+		error_message();
+
+	len_r = len_a + len_b;
+	if (len_a > len_b)
+		a = av[2], b = av[1], len_a = len_b, len_b = len_r - len_b;
+
+	result = adding_all_mul(a, len_a, b, len_b);
+
+	print_free_result(result);
+
 	return (0);
 }
