08b1a8aaa862c43c1a55b49732d194a8a8b2a1f4
ossai485 <104766197+ossai485@users.noreply.github.com>
2022-07-04 14:29:28 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - information about dogs
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
  */
 struct dog
 {
@@ -15,11 +17,10 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
+ * dog_t - Typedef for dog structure
  */
 typedef struct dog dog_t;
 
-int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
